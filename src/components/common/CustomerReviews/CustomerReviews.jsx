52c0c6026199887b3ea31663b645de05d43a96dc
Saadwarraich7223 <saadwarraich7223@gmail.com>
2026-01-03 23:09:35 +0500

    fixed reviewsection skelton

--- src/components/common/CustomerReviews/CustomerReviews.jsx
+++ src/components/common/CustomerReviews/CustomerReviews.jsx
@@ -64,13 +64,7 @@ const CustomerReviews = () => {
         </h2>
 
         {/* Loading Skeletons */}
-        {loading && (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
-            {[1, 2, 3].map((i) => (
-              <ReviewSkeleton key={i} />
-            ))}
-          </div>
-        )}
+        {loading && <ReviewSkeleton />}
 
         {/* No Reviews */}
         {!loading && topReviews.length === 0 && (
